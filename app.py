0aab83a8c63aec4b166ae4ba97b3b51b7aecab65
Emma Jennings <emma@0xem.ma>
2021-04-11 00:57:52 -0300

    Add Dir Size Code

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from typing import Sized
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_discord import DiscordOAuth2Session, requires_authorization, Unauthorized
-import os
+import os, subprocess
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/test.db'
 db = SQLAlchemy(app)
@@ -44,6 +44,7 @@ class Mirror(db.Model):
     href = db.Column(db.String(120), unique=True, nullable=False)
     size = db.Column(db.String(120), unique=False, nullable=False)
     synctime = db.Column(db.String(120), unique=False, nullable=False)
+    path = db.Column(db.String(120), unique=False, nullable=False)
 
     def __repr__(self):
         return '<Mirror Name %r>' % self.name
@@ -58,18 +59,35 @@ def index():
 @requires_authorization
 def admin():
     user = discord.fetch_user()
-    if user.id != "484040243818004491":
+    if user.id != 484040243818004491:
         abort(418)
+
     if request.method == "GET":
         Mirrors = Mirror.query.all()
-        return render_template('admin.html', user=user, mirrors=Mirrors)
+        return render_template('admin.html', user=user, mirrors=Mirrors, updatedSize=request.args.get('updatedSize'))
+
     if request.method == "POST":
-        mirrorEntry = Mirror(name=request.form['name'], href=request.form['href'], size=request.form['size'], synctime=request.form['synctime'])
+        mirrorEntry = Mirror(name=request.form['name'], href=request.form['href'], size=request.form['size'], synctime=request.form['synctime'], path=request.form['path'])
         db.session.add(mirrorEntry)
         db.session.commit()
         Mirrors = Mirror.query.all()
         return render_template('admin.html', user=user, mirrors=Mirrors)
 
+@app.route('/admin/updateSize', methods=["GET", "POST"])
+@requires_authorization
+def updateSize():
+    Mirrors = Mirror.query.all()
+    for mirror in Mirrors:
+        size = getSize(mirror.path)
+        mirror = Mirror.query.filter_by(name=mirror.name).first()
+        mirror.size = size
+        db.session.commit()
+    
+    return redirect(url_for('admin', updatedSize=True))
+
+def getSize(path):
+    size = subprocess.check_output(['du','-sh', path]).split()[0].decode('utf-8')
+    return size
 
 if __name__ == '__main__':
     init()
